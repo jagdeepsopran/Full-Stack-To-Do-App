91cb41217c8634da326728c79f19ed43fa4dcf26
Jagdeep <jagdeepsopran@gmail.com>
2025-01-08 09:43:33 +0530

    url

--- frontend/src/varibles.jsx
+++ frontend/src/varibles.jsx
@@ -1,2 +1,2 @@
-// export const baseURL = "https://todoappfullstack-kxv2.onrender.com";
-export const baseURL = "http://localhost:5000";
+export const baseURL = "https://full-stack-to-do-app-g8h5.onrender.com";
+// export const baseURL = "http://localhost:5000";
